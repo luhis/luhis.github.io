2a1bad3d288be0add9688d57fd16e16b16cf5f06
Matt McCorry <mccorry@gmail.com>
2020-11-11 20:20:26 +0000

    Updates

---
+++ component---src-pages-404-js-eb42f23bac8fa556f695.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("FdF9"),l=n("soUV"),u=n("hizP");t.default=function(){return a.default.createElement(l.a,null,a.default.createElement(u.a,{title:"404: Not found"}),a.default.createElement("h1",null,"NOT FOUND"),a.default.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-eb42f23bac8fa556f695.js.map
