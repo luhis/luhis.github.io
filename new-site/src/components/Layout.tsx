074614ec6ce0ceaf22ddf8fa87ba73b75e4b0690
Matt McCorry <mccorry@gmail.com>
2020-11-11 07:49:15 +0000

    added layouy back again

---
+++ new-site/src/components/Layout.tsx
@@ -0,0 +1,28 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import { Container } from "rbx"
+
+import "rbx/index.css"
+
+const Layout: React.FC<{ children: readonly JSX.Element[] }> = ({
+  children,
+}) => {
+  return (
+    <Container fluid>
+      <main>{children}</main>
+      <footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </footer>
+    </Container>
+  )
+}
+
+export default Layout
