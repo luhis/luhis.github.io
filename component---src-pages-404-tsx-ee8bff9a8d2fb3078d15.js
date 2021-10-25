91a0c32b96b223e01b684ee654f02b89b082bc8f
luhis <mccorry@gmail.com>
2021-10-25 08:18:13 +0000

    Deploying to built-pages from @ luhis/luhis.github.io@74cb508a55616abe0c04304335ea5771f368102f 🚀

---
+++ component---src-pages-404-tsx-ee8bff9a8d2fb3078d15.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[218],{938:function(e,t,a){a.r(t);var u=a(3731),l=a(5007),n=a(5871),s=a(5167);t.default=function(){return l.default.createElement(n.Z,null,l.default.createElement(s.Z,{title:"404: Not found"}),l.default.createElement(u.X6,null,"NOT FOUND"),l.default.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-ee8bff9a8d2fb3078d15.js.map
