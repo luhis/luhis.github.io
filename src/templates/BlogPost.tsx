b31eed1c456de5598965d1b826cdeb58b04c4ba3
Matt McCorry <mccorry@gmail.com>
2024-01-09 19:01:26 +0000

    a

--- src/templates/BlogPost.tsx
+++ src/templates/BlogPost.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import { graphql } from "gatsby";
+import { PageProps, graphql } from "gatsby";
 import { Columns, Content, Heading } from "react-bulma-components";
 
 import "prismjs/themes/prism-solarizedlight.css";
@@ -7,29 +7,26 @@ import "prismjs/themes/prism-solarizedlight.css";
 import Layout from "../components/Layout";
 import LeftCol from "../components/LeftCol";
 import SEO from "../components/Head";
-import { Post } from "../types/types";
 import BlogTags from "../components/BlogTags";
 
-const BlogPost: FC<{ readonly data: { readonly markdownRemark: Post } }> = ({
-  data,
-}) => {
+const BlogPost: FC<PageProps<Queries.BlogQueryQuery>> = ({ data }) => {
   const post = data.markdownRemark;
 
   return (
     <Layout>
-      <SEO title={`Matt McCorry's Blog ${post.frontmatter.title}`} />
+      <SEO title={`Matt McCorry's Blog ${post?.frontmatter?.title}`} />
       <Columns>
         <Columns.Column size={3}>
           <LeftCol />
         </Columns.Column>
         <Columns.Column>
           <article>
-            <Heading size={3}>{post.frontmatter.title}</Heading>
-            <BlogTags tags={post.frontmatter.tags} />
+            <Heading size={3}>{post?.frontmatter?.title}</Heading>
+            <BlogTags tags={post?.frontmatter?.tags || ""} />
             <Heading subtitle>
-              {post.frontmatter.author}, {post.frontmatter.date}
+              {post?.frontmatter?.author}, {post?.frontmatter?.date}
             </Heading>
-            <Content dangerouslySetInnerHTML={{ __html: post.html }} />
+            <Content dangerouslySetInnerHTML={{ __html: post?.html || "" }} />
           </article>
         </Columns.Column>
       </Columns>
